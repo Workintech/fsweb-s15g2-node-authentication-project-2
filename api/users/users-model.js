8b4204f3165c1b8ced2da086035795a4dddba2e0
EmreSahiner <34839005+EmreSahiner@users.noreply.github.com>
2023-02-14 15:31:14 +0300

    Update users-model.js

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -81,7 +81,7 @@ async function ekle({ username, password, role_name }) { // bu kısım hazır
     const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
     created_user_id = user_id
   })
-  return findById(created_user_id)
+  return idyeGoreBul(created_user_id)
 }
 
 module.exports = {
